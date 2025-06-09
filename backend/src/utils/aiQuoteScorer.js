2cc9e193214148ab803832ab219e37f713eb5a80
sarjukaneriya <135797218+sarjukaneriya@users.noreply.github.com>
2025-06-09 12:34:46 +0530

    Delete backend directory

--- backend/src/utils/aiQuoteScorer.js
+++
@@ -1,6 +0,0 @@
-const scoreQuote = (price, deliveryDays, rating = 5) => {
-  const score = 0.5 * price + 0.3 * deliveryDays + 0.2 * (1 - rating / 5);
-  return score;
-};
-
-export default scoreQuote;
